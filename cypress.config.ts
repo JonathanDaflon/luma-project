b15e2fb20eb99eb32e983b895ce8681cf4702090
Jonny Dragon <jonathandaflon@yahoo.com>
2024-11-05 00:31:54 -0300

    Fix: Trying to make CI work

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,22 @@ import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-prepro
 import { preprendTransformerToOptions } from "@badeball/cypress-cucumber-preprocessor/browserify";
 import browserify from "@cypress/browserify-preprocessor";
 
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    browserify({
+      ...preprendTransformerToOptions(config, browserify.defaultOptions),
+      typescript: require.resolve("typescript"),
+    })
+  );
+  return config;
+}
+
 export default defineConfig({
   e2e: {
     baseUrl: "https://www.lumahealth.io",
@@ -10,21 +26,6 @@ export default defineConfig({
     viewportWidth: 1280,
     viewportHeight: 720,
     specPattern: "**/*.feature",
-    async setupNodeEvents(
-      on: Cypress.PluginEvents,
-      config: Cypress.PluginConfigOptions
-    ): Promise<Cypress.PluginConfigOptions> {
-      await addCucumberPreprocessorPlugin(on, config);
-
-      on(
-        "file:preprocessor",
-        browserify({
-          ...preprendTransformerToOptions(config, browserify.defaultOptions),
-          typescript: require.resolve("typescript"),
-        })
-      );
-
-      return config;
-    },
+    setupNodeEvents,
   },
 });
